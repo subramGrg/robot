7ba48cb8443f487fb3fe83f1649b698a43bbb3a4
subram gurung <sups88@hotmail.com>
2017-06-18 08:37:05 +1000

    strange indentation. trying fixes

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,40 +1,40 @@
 module Robot
-  def self.helper; Robot::Helper; end
+	def self.helper; Robot::Helper; end
 
-  module Helper
-    # check validity of command and position
-	  def self.split input
-	    input = input.split
-    	command, position = input[0], input[1..-1].join
-	  end
+	module Helper
+		# check validity of command and position
+		def self.split input
+			input = input.split
+			command, position = input[0], input[1..-1].join
+		end
 
-    # check validity of position
-    def self.valid?(coordinates = {})
-      if !coordinates.nil?
-        return unless coordinates[:position] && coordinates[:face]
+		# check validity of position
+		def self.valid?(coordinates = {})
+			if !coordinates.nil?
+				return unless coordinates[:position] && coordinates[:face]
 
-        #check if face & coordinates are valid
-        @face = coordinates[:face]
-        face_valid? && position_valid?(coordinates[:position])
-      end
-    end
+				#check if face & coordinates are valid
+				@face = coordinates[:face]
+				face_valid? && position_valid?(coordinates[:position])
+			end
+		end
 
-    # check face is of accepted value
-    def self.face_valid?
-	    ['n', 's', 'w', 'e'].include? @face
-	  end
+		# check face is of accepted value
+		def self.face_valid?
+			['n', 's', 'w', 'e'].include? @face
+		end
 
-	  # check validity of co-ordinates
-	  def self.position_valid? coordinates
-      x, y = coordinates
+		# check validity of co-ordinates
+		def self.position_valid? coordinates
+			x, y = coordinates
 			begin
-	    	[x,y].all? do |coordinate|
+				[x,y].all? do |coordinate|
 					value = Integer(coordinate)
 					(0..5).include?(value)
 				end
 			rescue ArgumentError
-        false
+				false
 			end
-	  end
-  end
+		end
+	end
 end
