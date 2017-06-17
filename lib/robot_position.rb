6e09e44d710c374ca773cbed06e07060e86c9288
subram gurung <sups88@hotmail.com>
2017-06-18 08:52:36 +1000

    strange indentation. trying fixes

--- lib/robot_position.rb
+++ lib/robot_position.rb
@@ -1,64 +1,64 @@
 module Robot
   class << self
-  attr_reader :errors
+    attr_reader :errors
 
-  # position robot
-  def position input, &block
-  self.errors = nil
-  @command, @position = helper.split(input)
+    # position robot
+    def position input, &block
+      self.errors = nil
+      @command, @position = helper.split(input)
 
-  progress
-  end
+      progress
+    end
 
-  def progress
-  if place?
-  @x, @y, @face = @position.split(',')
-  @face.downcase!
+    def progress
+      if place?
+        @x, @y, @face = @position.split(',')
+        @face.downcase!
 
-  unless helper.valid?(position: [@x, @y], face: @face)
-  self.errors = 'Please provide valid position/coordinates'
-  end
-  else
-  self.errors = 'I only accept PLACE with co-ordinates'
-  end
-  end
+        unless helper.valid?(position: [@x, @y], face: @face)
+          self.errors = 'Please provide valid position/coordinates'
+        end
+      else
+        self.errors = 'I only accept PLACE with co-ordinates'
+      end
+    end
 
-  # check if command matches place
-  def place?
-  @command.downcase.eql? 'place'
-  end
+    # check if command matches place
+    def place?
+      @command.downcase.eql? 'place'
+    end
 
-  # move robot a unit in the direction it faces
-  def move_a_unit
-  case @face
-  when 'n' then
-  @y = @y.to_i + 1
-  when 's' then
-  @y = @y.to_i - 1
-  when 'e' then
-  @x = @x.to_i + 1
-  when 'w' then
-  @x = @x.to_i - 1
-  end
+    # move robot a unit in the direction it faces
+    def move_a_unit
+      case @face
+      when 'n' then
+        @y = @y.to_i + 1
+      when 's' then
+        @y = @y.to_i - 1
+      when 'e' then
+        @x = @x.to_i + 1
+      when 'w' then
+        @x = @x.to_i - 1
+      end
 
-  self.errors = 'Out of bounds' unless helper.position_valid? [@x, @y]
-  end
+      self.errors = 'Out of bounds' unless helper.position_valid? [@x, @y]
+    end
 
-  # change robots face
-  def face &block
-  @face = block.call(@face) if block_given?
-  end
+    # change robots face
+    def face &block
+      @face = block.call(@face) if block_given?
+    end
 
-  def errors?
-  errors.nil?
-  end
+    def errors?
+      errors.nil?
+    end
 
-  def errors= message
-  @errors = message
-  end
+    def errors= message
+      @errors = message
+    end
 
-  def coordinates
-  "#{@x}, #{@y}, #{@face}"
-  end
+    def coordinates
+      "#{@x}, #{@y}, #{@face}"
+    end
   end
 end
