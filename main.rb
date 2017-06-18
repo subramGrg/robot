75665a6aae0274b6ca0b307024a3764f39cba9a9
subram gurung <sups88@hotmail.com>
2017-06-18 13:33:18 +1000

    added test data on runtime

--- main.rb
+++ main.rb
@@ -1,33 +1,31 @@
 require './lib/robot_position'
 require './lib/robot_state'
 require './lib/helper'
-
+require 'byebug'
 require 'yaml'
-commands = YAML.load_file("./lib/input.yml")
 
 def main
-	prompt = gets.chomp
-	robot = Robot::State.new prompt
+  commands = YAML.load_file("./lib/input.yml")
 
-	puts robot.errors; return unless robot.valid?
-
-	command = 'y'
-	while !command.eql? 'n' do
-		command = gets.chomp
-		command.downcase!
-
-		case
-		when command.include?('place')
-			robot.position = command
-		when command.eql?('move')
-			robot.move
-		when command.eql?('left')
-			robot.left
-		when command.eql?('right')
-			robot.right
-		when command.eql?('report')
-			puts "\n\nRobots current position #{robot.report}\n\n\n"
-		else
-		end
-	end
+  # the following mimic's user entering commands
+  # however here commands are read from YAML instead
+  commands.each do |test_case|
+    puts "\n#{test_case[0]}"
+    test_case[1].each do |cmd|
+      case
+      when cmd.include?('place')
+        @robot = Robot::State.new cmd
+      when cmd.eql?('move')
+      	@robot.move
+      when cmd.eql?('left')
+      	@robot.left
+      when cmd.eql?('right')
+      	@robot.right
+      when cmd.eql?('report')
+      	puts "Robots current position => '#{@robot.report}'\n\n"
+      else
+      end
+    end
+  end
 end
+main
